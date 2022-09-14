8ee181fc5b0343bd5870094ebd3be00ed1986e97
Xenia Bogatko <bogatkok@gmail.com>
2022-09-14 23:17:44 +0200

    init first commit

---
+++ src/test/java/com/bogatkok/test1.java
@@ -0,0 +1,11 @@
+package com.bogatkok;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class test1 {
+    @Test
+    void testFirst(){
+        Assertions.assertTrue(100 > 1);
+    }
+}
