46fc85aaeb935fa0661e892263c5f36691dd835c
Xenia Bogatko <bogatkok@gmail.com>
2022-09-14 23:57:55 +0200

    random rules!

--- src/test/java/com/bogatkok/test1.java
+++ src/test/java/com/bogatkok/test1.java
@@ -3,13 +3,15 @@ package com.bogatkok;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+
 public class test1 {
     @Test
     void testFirst(){
-        Assertions.assertTrue(100 > 1);
+        Assertions.assertTrue(new Random().nextInt() > 1);
     }
     @Test
     void testSecond(){
-        Assertions.assertTrue(100 > 2);
+        Assertions.assertTrue(new Random().nextInt() > 2);
     }
 }
